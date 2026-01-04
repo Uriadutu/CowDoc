75e1368f0eb92eb73e66fd030bb43b11eb83e714
Uria Dutu <balisoan03@gmail.com>
2026-01-04 12:37:29 +0900

    icon

--- src/component/user/BerandaUser.jsx
+++ src/component/user/BerandaUser.jsx
@@ -75,7 +75,7 @@ const BerandaUser = () => {
             <p className="text-6xl mt-6">{jumlahDiagnosis}</p>
             <p className="text-xl mt-3">Jumlah Diagnosis</p>
           </div>
-          <img src={Diagnosis} className="h-full object-contain m-1 opacity-100 " alt="" />
+          <img src={Diagnosis} className="h-full object-contain m-1 opacity-30" alt="" />
         </div>
 
         {/* Penyakit Terbanyak */}
